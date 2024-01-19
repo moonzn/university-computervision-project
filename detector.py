bdf9c80d355a52cf6539ea5821232087d0c473f7
Gonçalo Carrasco <goncalo.carrasco@cloudalentejo.pt>
2024-01-19 23:07:19 +0000

    Início da implementação do Yolo

--- detector.py
+++ detector.py
@@ -1,5 +1,7 @@
 from ultralytics.models.yolo.detect import DetectionPredictor
 
-args = dict(model='yolo\\yolov8n-face.pt', source="assets")
+DATASET_FOLDER = ".crown_dataset_filtered"
+
+args = dict(model='yolo\\yolov8n-face.pt', source=DATASET_FOLDER)
 predictor = DetectionPredictor(overrides=args)
 predictor.predict_cli()
