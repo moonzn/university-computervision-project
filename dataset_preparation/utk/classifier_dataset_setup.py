35fee57e52a25af44b9630fe69f2ffb32d20b0fe
Gonçalo Carrasco <goncalo.carrasco@cloudalentejo.pt>
2024-01-23 18:19:47 +0000

    .

--- dataset_preparation/utk/classifier_dataset_setup.py
+++ dataset_preparation/utk/classifier_dataset_setup.py
@@ -2,13 +2,13 @@ import os
 import random
 import shutil
 
-utk = os.path.join(os.path.join(os.environ['USERPROFILE'], 'Desktop'), 'UTK')
+utk = os.path.join(os.path.join("C:\\Users\\Utilizador\\OneDrive - Group Sul", 'Ambiente de Trabalho'), 'UTK')
 ethnicity = ["white", "black", "asian", "indian", "others"]
 age_group = ["0-2", "3-7", "8-12", "13-19", "20-36", "37-65", "66+"]
 data_max = 0
 
 
-def ageGroupFinder(age):
+def age_group_finder(age):
     if 0 <= age < 3:
         return 0
     elif 3 <= age < 8:
@@ -41,7 +41,7 @@ def stats():
     print("The max number of photos per group for a balanced dataset is " + str(data_max))
 
 
-def preProcess():
+def pre_process():
     if os.path.exists(utk):
         if os.path.exists("./pre-processed"):
             shutil.rmtree("./pre-processed")
@@ -55,12 +55,12 @@ def preProcess():
         for filename in os.listdir(utk):
             ethn = int(filename.split('_')[2])
             age = int(filename.split('_')[0])
-            shutil.copy(utk + '/' + filename, "./pre-processed/" + ethnicity[ethn] + "/" + age_group[ageGroupFinder(age)])
+            shutil.copy(utk + '/' + filename, "./pre-processed/" + ethnicity[ethn] + "/" + age_group[age_group_finder(age)])
 
         stats()
 
 
-def datasetBuilder():
+def dataset_builder():
     if os.path.exists("../../datasets/utk"):
         shutil.rmtree("../../datasets/utk")
 
@@ -74,18 +74,18 @@ def datasetBuilder():
             if len(dir) < data_max:
                 for i in range(len(dir)):
                     id = dir[i].split('_')[3]
-                    age = str(ageGroupFinder(int(dir[i].split('_')[0])))
+                    age = str(age_group_finder(int(dir[i].split('_')[0])))
                     ethn = dir[i].split('_')[2]
                     shutil.copy("./pre-processed/" + e + "/" + a + "/" + dir[i], "../../datasets/utk/dataset")
                     f.write("{\"ID\": " + id + ", \"AGE\": " + age + ", \"ETHN\": " + ethn + "}\n")
             else:
                 for i in range(data_max):
                     id = dir[i].split('_')[3]
-                    age = str(ageGroupFinder(int(dir[i].split('_')[0])))
+                    age = str(age_group_finder(int(dir[i].split('_')[0])))
                     ethn = dir[i].split('_')[2]
                     shutil.copy("./pre-processed/" + e + "/" + a + "/" + dir[i], "../../datasets/utk/dataset")
                     f.write("{\"ID\": " + id + ", \"AGE\": " + age + ", \"ETHN\": " + ethn + "}\n")
 
 
-preProcess()
-datasetBuilder()
+pre_process()
+dataset_builder()
