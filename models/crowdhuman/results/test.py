6f3c3602fcaca41da6ae087d9c63a5cb2a148696
Gonçalo Carrasco <goncalo.carrasco@cloudalentejo.pt>
2024-01-24 13:22:49 +0000

    .

--- models/crowdhuman/results/test.py
+++ models/crowdhuman/results/test.py
@@ -26,17 +26,20 @@ def read_annotations_file():
     return data
 
 
-CROWDHUMAN_DIR = '..\\..\\..\\datasets\\crowdhuman\\dataset'
-CROWDHUMAN_ANNOTATIONS_PATH = '..\\..\\..\\datasets\\crowdhuman\\annotations\\crowdhuman_annotations'
-MODEL = 'yolov8n-face.pt'
+CROWDHUMAN_DIR = '../../../datasets/crowdhuman/dataset'
+CROWDHUMAN_ANNOTATIONS_PATH = '../../../datasets/crowdhuman/annotations/crowdhuman_annotations'
+MODEL = 'yolov8n-face.pt'  # 'yolov8m-face.pt'
+CONFIDENCE_THRESHOLD = 0.6  # Threshold for trust in bounding boxes
 IOU_THRESHOLD = 0.5  # Threshold for the IoU metric
 
 # Reading the annotations file
 annotations = read_annotations_file()
 
-# args = dict(model=MODEL, source=CROWDHUMAN_DIR)
-# predictor = DetectionPredictor(overrides=args)
-# predictor.predict_cli()
+#from ultralytics.models.yolo.detect import DetectionPredictor
+#args = dict(model=MODEL, source=CROWDHUMAN_DIR)
+#predictor = DetectionPredictor(overrides=args)
+#predictor.predict_cli()
+#exit(0)
 
 # Loading the model
 model = YOLO(MODEL)
@@ -44,15 +47,9 @@ model = YOLO(MODEL)
 # To calculate the metrics
 max_ious = []
 false_positives, true_positives = 0, 0
-total_boxes = 0
+total_images, total_boxes = 0, 0
 
 for file in os.listdir(CROWDHUMAN_DIR):
-    # Boxes of people's faces (ground truth)
-    img_id, _ = os.path.splitext(file)
-    img_boxes = annotations[img_id]
-    img_boxes = torch.tensor(img_boxes)
-    total_boxes += len(img_boxes)
-
     # Image reading and face detection
     path = os.path.join(CROWDHUMAN_DIR, file)
     img = cv.imread(path)
@@ -61,26 +58,41 @@ for file in os.listdir(CROWDHUMAN_DIR):
 
     prediction_boxes = []
     for box in boxes:
-        coord = box.xyxy[0]  # get box coordinates in (left, top, right, bottom) format
+        confidence = box.conf.item()
+        if confidence < CONFIDENCE_THRESHOLD:
+            continue
+        coord = box.xyxy[0]  # Get box coordinates in (left, top, right, bottom) format
         prediction_boxes.append(coord.tolist())
+
+    # Ignores the image if no face is detected
+    if len(prediction_boxes) == 0:
+        continue
+
     prediction_boxes = sorted(prediction_boxes, key=lambda x: x[0])
     prediction_boxes = torch.tensor(prediction_boxes)
 
+    # Boxes of people's faces (ground truth)
+    img_id, _ = os.path.splitext(file)
+    img_boxes = annotations[img_id]
+    img_boxes = torch.tensor(img_boxes)
+
     # Calculate intersection-over-union (IoU) of boxes
     # The pairwise IoU values for every element in 'img_boxes' and 'prediction_boxes'
-    iou_values = metrics.box_iou(img_boxes, prediction_boxes)
+    iou_values = metrics.box_iou(prediction_boxes, img_boxes)  # metrics.box_iou(img_boxes, prediction_boxes)
     # Maximum IoU value for each pair of boxes
     max_values, _ = torch.max(iou_values, dim=1)
     max_ious += max_values.tolist()
     true_positives += torch.sum(max_values >= IOU_THRESHOLD).item()  # Above or equal to the IoU threshold
     false_positives += torch.sum(max_values < IOU_THRESHOLD).item()
+    total_images += 1
+    total_boxes += len(img_boxes)
 
 # Display of metrics in relation to the total bounding boxes of the entire dataset
 precision = true_positives / (true_positives + false_positives)
 recall = true_positives / total_boxes
 f1_score = 2 * (precision * recall) / (precision + recall)
 average_iou = sum(max_ious) / len(max_ious)
-print(f"After {len(annotations)} images:")
+print(f"After {total_images} images:")  # len(annotations)
 print(f"Precision: {round(precision, 3)}"
       f" | Recall: {round(recall, 3)}"
       f" | F1-Score: {round(f1_score, 3)}"
