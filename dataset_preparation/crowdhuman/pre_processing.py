6eb30e0f875e779981d87209cf22ac2a917ac345
moonzn <raulmn.biz@gmail.com>
2024-01-29 14:12:19 +0000

    comments in pre_processing.py

--- dataset_preparation/crowdhuman/pre_processing.py
+++ dataset_preparation/crowdhuman/pre_processing.py
@@ -1,3 +1,28 @@
+"""
+Deep Learning for Computer Vision - Face detector and classifier by ethnicity and age group
+
+Preprocessing for the CrowdHuman dataset.
+
+Prerequisites:
+
+• "CrowdHuman" folder in the user's desktop containing a sample of the CrowdHuman dataset,
+we used training 1 through 3. Note that some images are inconsistently named and will
+cause errors.
+
+• An annotations file with the ODGT format (one JSON object per line), which has to have the annotations
+for the images in the "CrowdHuman" folder.
+
+Other datasets can be used as long as the Path constants are changed in global_variables.py.
+
+This script will read the images and their annotations and create a filtered dataset inside the project.
+The intent is to only get images that contain a number of people less than or equal to max_people_per_image.
+
+Authors:
+• Bernardo Grilo, n.º 93251
+• Gonçalo Carrasco, n.º 109379
+• Raúl Nascimento, n.º 87405
+"""
+
 from global_variables import *
 
 # User's Desktop path
@@ -46,7 +71,7 @@ with open(f'{RAW_CROWDHUMAN_ANNOTATIONS_PATH}', 'r') as file:
                 n_bounding_boxes += 1
 
         if n_bounding_boxes <= max_people_per_image:
-            # Cópia da imagem
+            # Copy the image into the project
             img_fn = f"{json_obj['ID']}.jpg"
             shutil.copy(f'{raw_crowdhuman_path}\\{img_fn}', f'{crowdhuman_dir}\\{img_fn}')
 
